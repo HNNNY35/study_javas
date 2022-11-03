2e58f2c47e1a9f35a053e4597e1d0cb473cbc159
HNNNY35 <hnnny35@gmail.com>
2022-11-03 15:10:47 +0900

    commit

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-<<<<<<< HEAD ###백준 Coding test
+###백준 Coding test
 |분류|주제|일반(코드)|함수(코드)|설명|
 |---|---|---|---|---|
-|사칙연산|덧셈|[링크](https://www.acmicpc.net/source/51271660)|함수(코드)|설명|
+|사칙연산|덧셈|[java](https://www.acmicpc.net/source/51271660)|[java](https://www.acmicpc.net/source/51271772)|두 정수를 합함|
+|사칙연산|덧셈|[java](https://www.acmicpc.net/source/51272503)|[java](https://www.acmicpc.net/source/51273066)|두 정수를 합함|
--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,55 @@
-public class Main {
-    public static void main(String[] args) {
-    import java.util.Scanner;
+import java.util.Scanner;
 
-// - 문제 파악
-//   ex) 두 정수 A와 B를 입력받은 다음 A+B를 출력
-//        입력값은 (0 < A, B < 10) 따라야 한다.
+// 문제 파악
+// 두 정수 A와 B를 입력받은 다음 A+B를 출력
+// 첫줄에 테스트 케이스의 개수 T
+// 각 테스트 케이스는 한 줄. 각 줄에 A와 B가 주어짐
+// (0 < A, B <10)
 
-// - 유추파악	
-//   ex) 두 정수를 입력해 합을 출력
-
-// - 주요 단어 영문 이름 선정
-//   ex) 입력(input), 정수(decimal), 출력(output), 합(plus)
-
-public class Main{
-    // 두 정수 합 구함
-    public int plus(int input_first, int input_second) {
-        int output_plus = input_first + input_second;
-        return output_plus;
-    }
-    public static void main(String[] args) {
-        // - 테스트 케이스
-        //   3 + 5 = 8
-        //   20 + 50 = 70
-        //   -1 + 5 = error
+// 유추 파악
+// 테스트 케이스 개수 입력받고
+        // 그 수만큼 A,B 입력받아서 출력
         
-        // - 프로그래밍 순서
-        //   입력
-        Scanner sc = new Scanner(System.in);
-        int input_first = sc.nextInt();
-        int input_second = sc.nextInt();
-        sc.close();
-
-        //   처리
-        Main main = new Main();
-        int output_plus = main.plus(input_first, input_second);
-
-        //   출력
-        System.out.println(output_plus);
+        // 주요 단어 영문 이름 선정
+        // 입력(input), 정수(demical), 출력(output), 합(plus)
         
-    }
-}
+        public class Main {
+        // 두 정수 합 구하기
+        public int plus(int input_first, int input_second) {
+            int output_plus = input_first + input_second;
+            return output_plus;
+        }
+
+        public static void main(String[] args) {
+        // 테스트 케이스
+        // 5
+        // 1 + 1 = 2
+        // 2 + 3 = 5
+        // -1 + 4 = error
+        
+        // 프로그래밍 순서
+        
+        // 입력
+        Scanner scanner = new Scanner(System.in);
+        int input_t = scanner.nextInt();
+        int input_first;
+        int input_second;
+        int output_plus;
+        for(int i = 0; i < input_t; i++){
+            input_first = scanner.nextInt();
+            input_second = scanner.nextInt();
+            
+            // 처리
+            Main main = new Main();
+            output_plus = main.plus(input_first, input_second);
+
+            // 출력
+            System.out.println(output_plus);
+        }
+
+        // try-on
+        // 실패 시 원인 기록
+        
+    
 }
 }

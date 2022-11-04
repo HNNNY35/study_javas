4444dd864293753fef8b09da1bc3d70326e0157b
HNNNY35 <hnnny35@gmail.com>
2022-11-04 13:36:17 +0900

    bj

--- README.md
+++ README.md
@@ -5,3 +5,4 @@
 | 사칙연산 | 덧셈 | [java](https://www.acmicpc.net/source/51271660) | [java](https://www.acmicpc.net/source/51271772) | 두 정수를 합함             |
 | 사칙연산 | 덧셈 | [java](https://www.acmicpc.net/source/51272503) | [java](https://www.acmicpc.net/source/51273066) | 두 정수를 합하는 것을 반복 |
 | 사칙연산 | 곱셈 | [java](https://www.acmicpc.net/source/51305648) | [java](https://www.acmicpc.net/source/51305765) | 두 정수를 곱함             |
+| 사칙연산 | 합   | [java](https://www.acmicpc.net/source/51308080) | [java]()                                        | 1부터 N까지 합함           |
--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,31 @@
 import java.util.Scanner;
 
-// 문제 파악
-// 두 정수 A, B 입력받은 후 AxB 출력(0 < A, B < 10)
+// baekjoon_8393_analysis.txt
+ public class Main {
+    // - 테스트 케이스
+    // 3 --> 6
+    // 5 --> 15
+    // 1 --> 1
 
-// 유추 파악
-// A,B 입력받아서 AxB출력
-        
-// 주요 단어 영문 이름 선정
-// 입력(input), 정수(demical), 출력(output), 곱하기(multiplied)
-        
-        public class Main {
-        public int multiplied(int input_first, int input_second) {
-            int multiplied = input_first * input_second;
-            return multiplied;
+    public int sum(int input) {
+        int sum = 0;
+        for(int i = 1; i <= input; i++) {
+            sum += i;
         }
+        return sum;
+    }
 
-        public static void main(String[] args) {
-        // 테스트 케이스
-        // 1 x 2 = 2
+    public static void main(String[] args) {
+        // 반복값 입력
+        Scanner scanner = new Scanner(System.in);
+        int input = scanner.nextInt();
+        scanner.close();
         
-        // 프로그래밍 순서
-        // 입력
-       Scanner scanner = new Scanner(System.in);
-       int input_first = scanner.nextInt();
-       int input_second = scanner.nextInt();
-
-        // 처리
+        // 합 구함
         Main main = new Main();
-        int multiplied = main.multiplied(input_first, input_second);
+        int output = main.sum(input);
 
-        // 출력
-        System.out.println(multiplied);
+        // 합한 값 출력
+        System.out.println(output);
         }
-
-        // try-on
-        // 실패 시 원인 기록
 }

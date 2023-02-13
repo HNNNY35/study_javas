52a0d65fd2fb60c2f37f57fd3594ec47a9928499
HNNNY35 <hnnny35@gmail.com>
2023-02-13 13:55:11 +0900

    practice

---
+++ src/ApiExplorersWithDatagoKr_practice.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+
+public class ApiExplorersWithDatagoKr_practice {
+    public static void main(String[] args) throws UnsupportedEncodingException {
+        /*
+        행정안전부_지진해일 긴급대피장소
+        http://apis.data.go.kr/1741000/TsunamiShelter3/getTsunamiShelter1List?serviceKey=5xenYzMP2aszyTViKczyzw3eY12K2UXe6Dp8rvmi43TVwg6%2FUPzUt9fGN9CZ6c8bTQ4cPFzUGUZCtBpn%2FZTnBg%3D%3D&pageNo=1&numOfRows=10&type=xml
+        */
+        
+        StringBuilder urlBuilder = new StringBuilder("https://apis.data.go.kr/1741000/TsunamiShelter3/getTsunamiShelter1List"); /*URL*/
+        // 2. 오픈 API의요청 규격에 맞는 파라미터 생성, 발급받은 인증키.
+        urlBuilder.append("?" + URLEncoder.encode("ServiceKey","UTF-8") + "=" + URLEncoder.encode("5xenYzMP2aszyTViKczyzw3eY12K2UXe6Dp8rvmi43TVwg6%2FUPzUt9fGN9CZ6c8bTQ4cPFzUGUZCtBpn%2FZTnBg%3D%3D")); /*Service Key*/\
+        urlBuilder.append("&" + URLEncoder.encode("pageNo","UTF-8") + "=" + URLEncoder.encode("1", "UTF-8"));
+        urlBuilder.append("&" + URLEncoder.encode("numOfRows","UTF-8") + "=" + URLEncoder.encode("10", "UTF-8"));
+        urlBuilder.append("&" + URLEncoder.encode("type","UTF-8") + "=" + URLEncoder.encode("xml", "UTF-8"));
+
+
+        // 연결 버튼
+        URL url;
+        HttpURLConnection conn = null;
+        try {
+            url = new URL(urlBuilder.toString());
+            conn = (HttpURLConnection)url.openConnection();
+            conn.setRequestMethod("GET");
+            // conn.setRequestProperty(null, null);
+            System.out.println("ResponseCode : "+ conn.getResponseCode() );
+
+            BufferedReader reader;
+            if(conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
+                reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            } else {
+                reader = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+            }
+            StringBuffer stringBuffer = new StringBuffer();
+            String line = "";
+            while((line = reader.readLine()) != null){
+                stringBuffer.append(line);
+            }
+            int i = 1;
+            reader.close();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            conn.disconnect();
+        }
+    }
+}
